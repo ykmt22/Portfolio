90e7a42effb0b86d91dcbe0b02d8d954b8ead3f6
Shashank Maurya <shashankmaurya260101@gmail.com>
2023-05-31 13:42:24 +0530

    image downscaling commit

--- photography/photography.js
+++ photography/photography.js
@@ -1,7 +1,9 @@
+// import fs from 'fs';
+// var proxyURL = 'https://cors-anywhere.herokuapp.com/';
 var img_links = [];
 let slideIndex = 0;
 
-$.getJSON('https://script.google.com/macros/s/AKfycbxqJU1pQP4xjOci0GQ7DY8MtvhxIv2Ikac2v0dIeLQ34f4t4luneim1_EhZrSUTYMmHWA/exec', getImages);
+$.getJSON('https://script.google.com/macros/s/AKfycbwbe8X7rSHrhMdTEz3Gj0QeB4DmkJV9rOeA_V6J-0fWM4fo-nMVaqJ_hQRioQh18PXwtg/exec', getImages);
 
 function getImages(img_json) {
     console.log(img_json);
@@ -22,7 +24,7 @@ function getImages(img_json) {
 
 // Carousel Controller
 
-function carouselBuilder() {
+function carouselBuilder(img_links) {
 
     console.log(img_links.length);
 
@@ -32,35 +34,21 @@ function carouselBuilder() {
         var newImg = document.createElement('img');
         newImg.className = 'cover_image';
         newImg.width = "800";
-        newImg.height = "480";
-        newImg.src = img_links[i];
-
-        // manipulate image here
-
-        
-
-        // const image = new Image();
-        // image.src = newImg.src;
-        // console.log(image.src);
-
-        // var canvas = document.createElement('canvas');
-        // canvas.width = newImg.width;
-        // canvas.height = newImg.height;
-
-        // const ctx = canvas.getContext('2d');
-        // ctx.drawImage(newImg, 0, 0, newImg.width, newImg.height)
-
-        // canvas.toBlob(function(blob){
-            // console.log(blob);
-            // newImg.src = URL.createObjectURL(blob);
-            // console.log(newImg.src);
-        // }, newImg.mimeType, 50);
+        newImg.height = "500";
+        newImg.src = compressImg(img_links[i], `${newImg.width}x${newImg.height}`, 'low');
+        // setTimeout(function () {
+        //     var API_URL = `https://img.gs/lkkrljgcxx/${newImg.width}x${newImg.height},quality=low/${img_links[i]}`;
+        //     console.log(API_URL);
+        //     newImg.src = API_URL;
+        // }, 2000);
+        newImg.referrerPolicy = 'no-referrer';
 
         var chidDiv = document.createElement('div')
         chidDiv.className = "mySlides fade";
-        chidDiv.id = 'slide_'+(i+1);
+        chidDiv.id = 'slide_' + (i + 1);
         chidDiv.innerHTML = `<div class="numbertext"> ${i + 1} / ${img_links.length} </div>`;
         chidDiv.appendChild(newImg);
+
         // chidDiv.appendChild(canvas);
 
         // newDiv.innerHTML = `<div class="mySlides fade" id="slide_${i + 1}">
@@ -72,17 +60,58 @@ function carouselBuilder() {
         newDiv.appendChild(chidDiv);
         newDiv.classList.add('parentSlide')
 
-        document.querySelector('div.slideshow').appendChild(newDiv);
+        // document.querySelector('div.slideshow').appendChild(newDiv);
+        $('div.slideshow').append(newDiv);
     }
+
+}
+
+function compressImg(imageURL, dimentions, quality) {
+    let API_URL = `https://img.gs/lkkrljgcxx/${dimentions},quality=${quality}/${imageURL}`;
+
+    // $.get(API_URL, function(data, status){
+    //     console.log("data: "+data);
+    //     console.log("status: "+status);
+    // });
+
+    console.log(API_URL);
+
+    return API_URL;
+
+    // var request = $.ajax({
+    //     url: API_URL,
+    //     type: 'GET',
+    //     crossDomain: true,
+    //     contentType: 'image/jpeg',
+    //     dataType: 'jsonp',
+    //     xhrFields: {
+    //         responseType: 'blob'
+    //     },
+    //     beforeSend: function (xhr) {
+    //         xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+    //     },
+    //     success: function (data, status) {
+    //         console.log('data: ' + data);
+    //         console.log('status: ' + status);
+    //         const url = window.URL || window.webkitURL;
+    //         const src = url.createObjectURL(data);
+    //         console.log(src);
+    //     },
+    //     error: function (data, status) {
+    //         console.log('ERROR data: ' + data);
+    //         console.log('ERROR status: ' + status);
+    //     }
+    // });
 
 }
 
 function showSlidesAuto() {
     let i;
     let slides = document.querySelectorAll(".mySlides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
+    $('.mySlides').css('display', 'none');
+    // for (i = 0; i < slides.length; i++) {
+    //     slides[i].style.display = "none";
+    // }
     slideIndex++;
     if (slideIndex > slides.length) {
         slideIndex = 1
@@ -93,16 +122,17 @@ function showSlidesAuto() {
 
 function showSlides(n) {
     let i;
-    let slides = document.getElementsByClassName("mySlides");
-    if (n > slides.length) { 
-        slideIndex = 1 
+    let slides = document.querySelectorAll(".mySlides");
+    if (n > slides.length) {
+        slideIndex = 1
     }
-    if (n < 1) { 
-        slideIndex = slides.length 
+    if (n < 1) {
+        slideIndex = slides.length
     }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
+    // for (i = 0; i < slides.length; i++) {
+    //     slides[i].style.display = "none";
+    // }
+    $('.mySlides').css('display', 'none');
     slides[slideIndex - 1].style.display = "block";
 }
 
